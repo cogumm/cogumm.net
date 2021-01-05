a81ac13270e559cf531bc91523544ba1a1ea922c
Gabriel F. Vilar <gabriel@cogumm.net>
2021-01-05 12:28:33 -0300

    :sparkles: Add SEO component

--- 2021/src/pages/_document.tsx
+++ 2021/src/pages/_document.tsx
@@ -1,4 +1,10 @@
-import Document, { DocumentContext } from "next/document";
+import Document, {
+    DocumentContext,
+    Html,
+    Head,
+    Main,
+    NextScript,
+} from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
@@ -26,5 +32,20 @@ export default class MyDocument extends Document {
         } finally {
             sheet.seal();
         }
+    }
+
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <meta charSet="utf-8" />
+                </Head>
+
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
     }
 }
--- 2021/src/pages/index.tsx
+++ 2021/src/pages/index.tsx
@@ -1,6 +1,9 @@
+import SEO from "~/components/SEO";
+
 export default function Home() {
     return (
         <div>
+            <SEO title="Gabriel 'CoGUMm' Vilar" image="image/cgm.png" />
             <h1>In progress</h1>
         </div>
     );
